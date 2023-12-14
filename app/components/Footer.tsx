49c820c3317e409c69603d1df1723b6226c65a5c
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-12-14 09:15:01 +0100

    change footer link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -18,7 +18,7 @@ export default function Footer({ className = '' }: { className: string }) {
       <br />
       <a
         className="footer-link"
-        href="https://github.com/tatasadi/stats-preview-card-component"
+        href="https://github.com/tatasadi/testimonials-grid-section"
         target="_blank"
         rel="noreferrer"
       >
